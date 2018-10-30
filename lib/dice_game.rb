701188faccf8f0378e722eefbf1a8c75eed538ee
Max Oliver Francis <maxfrancis@Maxs-MacBook-Pro.local>
2018-10-30 09:37:28 +0000

    Finished the last user story and added appropriate testing

--- lib/dice_game.rb
+++ lib/dice_game.rb
@@ -1,9 +1,15 @@
 class Dice_game
 
-  def roll(num)
-    num.times do
-      puts rand(1...7)
+  def dice
+    possibilities = [1, 2, 3, 4, 5, 6].sample
+  end
+
+  def roll(x)
+    dice_rolls = []
+    x.times do
+       dice_rolls.push(dice)
     end
+    dice_rolls
   end
 
 end
--- spec/dice_game_spec.rb
+++ spec/dice_game_spec.rb
@@ -5,11 +5,13 @@ describe Dice_game do
   describe '#roll' do
 
     it 'Rolls a dice' do
-      expect(subject.roll(1)).to be_between(1,6)
+      expect(subject.dice).to be_between(1,6)
     end
 
-    it 'Rolls three dice' do
-      expect(subject.roll(3)).to be_between(1,6)
+    it 'Rolls Two dice' do
+      output = subject.roll(2)
+      expect(output[0]).to be_between(1, 6)
+      expect(output[1]).to be_between(1, 6)
     end
 
   end
